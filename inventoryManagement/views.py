19eea35de1b4971d50d09ba764c7db2ffe8fb876
devanks97 <devanks97@gmail.com>
2018-06-06 13:17:59 +0400

    Added error pages

--- inventoryManagement/views.py
+++ inventoryManagement/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def error_404(request):
+        data = {}
+        return render(request,'inventoryManagement/error_404.html', data)
+ 
+def error_500(request):
+        data = {}
+        return render(request,'inventoryManagement/error_500.html', data)
