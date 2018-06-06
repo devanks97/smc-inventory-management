5a31e4627d293d9b38afc263a753fc3c9eb1629c
devanks97 <devanks97@gmail.com>
2018-06-06 11:03:59 +0400

    Default table appearing attempt v1

--- inventoryManagement/ModuleSummaryAdminSettings.py
+++ inventoryManagement/ModuleSummaryAdminSettings.py
@@ -7,21 +7,23 @@ class recordSummaryAdmin(admin.ModelAdmin):
     list_filter = (
         'device','department','year'
     )
-    def changelist_view(self, request, extra_context=None):
-        response = super().changelist_view(
-        request,
-        extra_context=extra_context,
-        )
-        try:
-            qs = response.context_data['cl'].queryset
-        except (AttributeError, KeyError):
-            return response
+    # def changelist_view(self, request, extra_context=None):
+        # response = super().changelist_view(
+        # request,
+        # extra_context=extra_context,
+        # )
+        # try:
+            # qs = response.context_data['cl'].queryset
+        # except (AttributeError, KeyError):
+            # return response
 	
-        response.context_data['summary'] = list(
-        qs
-        .values('department','device')
-        .annotate(total=Count('id'))
-        .order_by('total')
-        )
-        return response
- 
+        # response.context_data['summary'] = list(
+        # qs
+        # .values('department','device')
+        # .annotate(total=Count('id'))
+        # .order_by('total')
+        # )
+        # return response
+    def get_queryset(self, request):
+        qs = super(recordSummaryAdmin, self).get_queryset(request)
+        return qs.values('department','device').annotate(total=Count('id')).order_by('total')
