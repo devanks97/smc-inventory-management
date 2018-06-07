69cf9c3a9a408ccd0f882feda850825f6c6444bc
devanks97 <devanks97@gmail.com>
2018-06-07 11:44:46 +0400

    Debug enviro variable set

--- SMCManagement/settings.py
+++ SMCManagement/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't$ct%q$*2h=yegf_yn@1g$a+!^w4s-g3*pcx-nv7$nhrr1nqe%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', ''))
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+
 ALLOWED_HOSTS = [*]
 
 #CHECK IF DEBUG
