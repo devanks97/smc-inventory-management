bc46f3fa40ee8c7b050d9a518fee68a730a24505
devanks97 <devanks97@gmail.com>
2018-06-07 11:19:16 +0400

    Use enivroment variable from heroku v3

--- SMCManagement/settings.py
+++ SMCManagement/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't$ct%q$*2h=yegf_yn@1g$a+!^w4s-g3*pcx-nv7$nhrr1nqe%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEV_DEBUG')
+ENV = 'DEV_DEBUG'
+DEBUG = os.environ.get(ENV)
 ALLOWED_HOSTS = ['smc-inventory-management.herokuapp.com','smc-inventory-management-beta.herokuapp.com','127.0.0.1']
 
 #CHECK IF DEBUG
