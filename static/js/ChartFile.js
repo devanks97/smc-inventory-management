7a04898941513f52647c564a505edd3d6031a9c2
devanks97 <39550710+devanks97@users.noreply.github.com>
2018-06-11 10:05:54 +0400

    Added onload function to chartjs

--- static/js/ChartFile.js
+++ static/js/ChartFile.js
@@ -1,117 +1,119 @@
-tbodyTaggedElements = document.getElementsByTagName("tbody")[1]
-trTaggedElements = tbodyTaggedElements.children;
-length = trTaggedElements.length; 
-var i;
-var j;
+window.onload = function(){
+	tbodyTaggedElements = document.getElementsByTagName("tbody")[1]
+	trTaggedElements = tbodyTaggedElements.children;
+	length = trTaggedElements.length; 
+	var i;
+	var j;
 
-				// let rowInfo = [];
-				// function addRecord(department, device, total) {
-				  // rowInfo.push({department, device, total});
-				// }
-let labelList = [];
-				// function addLabel(department) {
-				  // labelList.push({department});
-				// }
-				// let PCList = [];
-				// function addDataset(total) {
-				  // datasetList.push({total});
-				// }
+					// let rowInfo = [];
+					// function addRecord(department, device, total) {
+					  // rowInfo.push({department, device, total});
+					// }
+	var labelList = [];
+					// function addLabel(department) {
+					  // labelList.push({department});
+					// }
+					// let PCList = [];
+					// function addDataset(total) {
+					  // datasetList.push({total});
+					// }
 
-				// let LaptopList = [];
-				// function addDataset(total) {
-				  // datasetList.push({total});
-				// }
+					// let LaptopList = [];
+					// function addDataset(total) {
+					  // datasetList.push({total});
+					// }
 
-				// let AIOList = [];
+					// let AIOList = [];
 
 
-for(i=0;i<length;i++)
-{
-	
-	tempRow = trTaggedElements[i];
-	tempDepartment = tempRow.children[0].innerText;
-	tempDevice = tempRow.children[1].innerText;
-	tempTotal = tempRow.children[2].innerText;
-				// addRecord(tempRow.children[0].innerText,tempRow.children[1].innerText,tempRow.children[2].innerText);
-				// labelList.push(tempRow.children[0].innerText);
-				// if(tempRow.children[1].innerText == "Personal Computer")
-					// PCList.push(tempRow.children[2].innerText);
-				// else if(tempRow.children[1].innerText == "Laptop")
-					// LaptopList.push(tempRow.children[2].innerText);
-				// else
-					// AIOList.push(tempRow.children[2].innerText);
-	if(!labelList[tempDepartment])
+	for(i=0;i<length;i++)
 	{
-		labelList[tempDepartment] = {lt:0,pc:0,aio:0};
-	}
-	
-	if(tempDevice == "Laptop")
-	{
-		labelList[tempDepartment].lt = tempTotal;
-	}
-	else if(tempDevice == "Personal Computer")
-	{
-		labelList[tempDepartment].pc = tempTotal;
-	}
-	else
-	{
-		labelList[tempDepartment].aio = tempTotal;
-	}
-
-}
-
-var departmentList = [];
-var departmentListPC = [];
-var departmentListLaptop = [];
-var departmentListAIO = [];
-for(var key in labelList) {
-  if(labelList.hasOwnProperty(key)) 
-  { //to be safe
-	departmentListPC.push(labelList[key].pc);
-	departmentListLaptop.push(labelList[key].lt);
-	departmentListAIO.push(labelList[key].aio);
-	departmentList.push(key);
-  }
-}
-
-
-
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: departmentList,
-        datasets: [{
-            label: 'PC',
-            data: departmentListPC,
-            backgroundColor:'rgba(255, 99, 132, 0.2)',
-            borderColor:'rgba(255,99,132,1)',
-            borderWidth: 1
-        },
-		{
-            label: 'Laptop',
-            data: departmentListLaptop,
-            backgroundColor:'rgba(54, 162, 235, 0.2)',
-            borderColor:'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        },
-		{
-            label: 'All In One',
-            data: departmentListAIO,
-            backgroundColor:'rgba(255, 206, 86, 0.2)',
-            borderColor:'rgba(255, 206, 86, 1)',
-            borderWidth: 1
-        }
 		
-		]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+		tempRow = trTaggedElements[i];
+		tempDepartment = tempRow.children[0].innerText;
+		tempDevice = tempRow.children[1].innerText;
+		tempTotal = tempRow.children[2].innerText;
+					// addRecord(tempRow.children[0].innerText,tempRow.children[1].innerText,tempRow.children[2].innerText);
+					// labelList.push(tempRow.children[0].innerText);
+					// if(tempRow.children[1].innerText == "Personal Computer")
+						// PCList.push(tempRow.children[2].innerText);
+					// else if(tempRow.children[1].innerText == "Laptop")
+						// LaptopList.push(tempRow.children[2].innerText);
+					// else
+						// AIOList.push(tempRow.children[2].innerText);
+		if(!labelList[tempDepartment])
+		{
+			labelList[tempDepartment] = {lt:0,pc:0,aio:0};
+		}
+		
+		if(tempDevice == "Laptop")
+		{
+			labelList[tempDepartment].lt = tempTotal;
+		}
+		else if(tempDevice == "Personal Computer")
+		{
+			labelList[tempDepartment].pc = tempTotal;
+		}
+		else
+		{
+			labelList[tempDepartment].aio = tempTotal;
+		}
+
+	}
+
+	var departmentList = [];
+	var departmentListPC = [];
+	var departmentListLaptop = [];
+	var departmentListAIO = [];
+	for(var key in labelList) {
+	  if(labelList.hasOwnProperty(key)) 
+	  { //to be safe
+		departmentListPC.push(labelList[key].pc);
+		departmentListLaptop.push(labelList[key].lt);
+		departmentListAIO.push(labelList[key].aio);
+		departmentList.push(key);
+	  }
+	}
+
+
+
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: departmentList,
+			datasets: [{
+				label: 'PC',
+				data: departmentListPC,
+				backgroundColor:'rgba(255, 99, 132, 0.2)',
+				borderColor:'rgba(255,99,132,1)',
+				borderWidth: 1
+			},
+			{
+				label: 'Laptop',
+				data: departmentListLaptop,
+				backgroundColor:'rgba(54, 162, 235, 0.2)',
+				borderColor:'rgba(54, 162, 235, 1)',
+				borderWidth: 1
+			},
+			{
+				label: 'All In One',
+				data: departmentListAIO,
+				backgroundColor:'rgba(255, 206, 86, 0.2)',
+				borderColor:'rgba(255, 206, 86, 1)',
+				borderWidth: 1
+			}
+			
+			]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+}
