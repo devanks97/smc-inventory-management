f2a64feaeb36f781191c5c1c820d07253202f902
devanks97 <39550710+devanks97@users.noreply.github.com>
2018-06-13 13:35:54 +0400

    Secured production level faults

--- SMCManagement/settings.py
+++ SMCManagement/settings.py
@@ -155,6 +155,11 @@ USE_TZ = True
 #Change session engine to cookie based
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
+# Production Level Changes
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
