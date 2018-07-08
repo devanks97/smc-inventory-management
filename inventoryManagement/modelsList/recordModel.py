61ebc5bcf911ca5b18b1f0c0f114ecf756660f7f
devanks97 <39550710+devanks97@users.noreply.github.com>
2018-07-08 13:12:53 +0400

    Validate devicetag before save

--- inventoryManagement/modelsList/recordModel.py
+++ inventoryManagement/modelsList/recordModel.py
@@ -8,7 +8,7 @@ from django.core.validators import MinValueValidator
 
 class record(models.Model):
     name = TitleCaseCharField(max_length=200,validators=[validate_correct_text])
-    department = TitleCaseCharField(max_length=50,validators=[validate_correct_text])
+    department = UpperCaseCharField(max_length=50,validators=[validate_correct_text])
     location = TitleCaseCharField(max_length=50,validators=[validate_correct_text])
     year = models.DecimalField(max_digits=4,decimal_places=0,validators=[MinValueValidator(2016)])
     device = models.ForeignKey(deviceList,on_delete=models.CASCADE)
@@ -19,3 +19,15 @@ class record(models.Model):
     class Meta:
         verbose_name = 'Record Of Inventory'
         verbose_name_plural = 'Records Of Inventory'
+
+import re
+deviceTagPattern = re.compile("^([a-zA-z])(-)((?:[a-zA-Z][a-zA-Z]+))(-)((?:[a-zA-Z][a-zA-Z]+))(-)(\d+)$")
+
+def validate_deviceTag_pre_save(sender, instance, **kwargs):
+    instanceDeviceTag = instance.deviceTag
+    if not deviceTagPattern.match(instanceDeviceTag):
+        from django.core.exceptions import ValidationError
+        raise ValidationError(
+            'DeviceTag "{}" is not followinf the format'.format(instanceDeviceTag),code='deleting_protected')
+
+models.signals.pre_save.connect(validate_deviceTag_pre_save, sender=record)
