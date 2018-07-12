493c09c1f1ffc51a3c56df4fe8704d79210c5654
devanks97 <39550710+devanks97@users.noreply.github.com>
2018-07-12 11:51:47 +0400

    Better field views

--- inventoryManagement/adminFiles/ModuleRecordAdmin.py
+++ inventoryManagement/adminFiles/ModuleRecordAdmin.py
@@ -4,7 +4,7 @@ from .ModuleCachingPaginator import CachingPaginator
 
 class recordAdmin(ImportExportActionModelAdmin):
     resource_class = RecordResource
-    list_display = ['name', 'department', 'year', 'device','deviceTag','building','old_device_tag','item_description','serial_no','brand','model_no','service_tag','floor','room','remarks']
+    list_display = ['name', 'department', 'year', 'device','deviceTag']
     list_filter = ('department', 'year', 'device')
     search_fields = ('name', 'department','deviceTag','old_device_tag')
     show_full_result_count = False
